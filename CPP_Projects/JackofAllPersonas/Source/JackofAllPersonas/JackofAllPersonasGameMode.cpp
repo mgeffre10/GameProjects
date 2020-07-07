197d49fe89bcf741914abe43ffa73f3dc040f251
Michael Geffre <geffrejunior92@gmail.com>
2020-07-07 12:06:15 -0700

    Updating Gamemode and Default Map

--- CPP_Projects/JackofAllPersonas/Source/JackofAllPersonas/JackofAllPersonasGameMode.cpp
+++ CPP_Projects/JackofAllPersonas/Source/JackofAllPersonas/JackofAllPersonasGameMode.cpp
@@ -1,15 +1,9 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 #include "JackofAllPersonasGameMode.h"
-#include "JackofAllPersonasCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AJackofAllPersonasGameMode::AJackofAllPersonasGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	
 }
