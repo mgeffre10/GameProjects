76ad1e2f6e507b46a5ac59eb17f05d666de54f4a
Michael Geffre <geffrejunior92@gmail.com>
2020-07-15 16:43:12 -0700

    Initial Commit

--- CPP_Projects/JackofAllPersonas/Source/JackofAllPersonas/JackofAllPersonasGameMode.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "JackofAllPersonasGameMode.h"
-#include "UObject/ConstructorHelpers.h"
-
-AJackofAllPersonasGameMode::AJackofAllPersonasGameMode()
-{
-	
-}
