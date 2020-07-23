372bac56ef88d9b3c5d2a182b4e6ac2de31a4479
Michael Geffre <geffrejunior92@gmail.com>
2020-07-22 17:03:37 -0700

    Initial Commit

---
+++ CPP_Projects/GameExperienceOne/Source/GameExperienceOne/GameExperienceOne.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "GameExperienceOne.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameExperienceOne, "GameExperienceOne" );
+ 
